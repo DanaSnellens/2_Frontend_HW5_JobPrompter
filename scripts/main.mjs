d4756b496f04b2d092a9718d62e8c57627e930ea
DanaSnellens <danasnellens@gmail.com>
2024-04-02 18:37:17 +0200

    Opdracht 3 AF

--- scripts/main.mjs
+++ scripts/main.mjs
@@ -40,3 +40,25 @@ const userInput = prompt("Je koos voor marketing. Over welke functie wil je meer
      +  "\r 2: " + departments.marketing.jobs[2].title + " \r 3: "+ departments.marketing.jobs[3].title +
     " \r 4: "+ departments.marketing.jobs[4].title);
 
+//OPDRACHT 3B & 3C
+switch (userInput) {
+    case "0":
+        console.log("Je koos " + departments.marketing.jobs[0].title + ". Een uitdagende rol! " + departments.marketing.jobs[0].description);
+        break;
+    case "1":
+        console.log("Je koos " + departments.marketing.jobs[1].title + ". Een uitdagende rol! " + departments.marketing.jobs[1].description);
+        break;
+    case "2":
+        console.log("Je koos " + departments.marketing.jobs[2].title + ". Een uitdagende rol! " + departments.marketing.jobs[2].description);
+        break;
+    case "3":
+        console.log("Je koos " + departments.marketing.jobs[3].title + ". Een uitdagende rol! " + departments.marketing.jobs[3].description);
+        break;
+    case "4":
+        console.log("Je koos " + departments.marketing.jobs[4].title + ". Een uitdagende rol! " + departments.marketing.jobs[4].description);
+        break;
+
+    default:
+        console.error("Ongeldige keuze. Probeer het opnieuw door de pagina te verversen");
+
+}
